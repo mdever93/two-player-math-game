1cd115ce6115ecafd2baa8858ad2b52a551fea3f
Michael Dever <mdever@live.ca>
2022-01-31 17:47:31 -0500

    game class, question class

---
+++ classes/Game.rb
@@ -0,0 +1,7 @@
+class Game
+  def initialize
+    @current_player = nil
+    @players = []
+    @current_round = 1
+  end
+end
---
+++ classes/Question.rb
@@ -0,0 +1,8 @@
+class Question
+  attr_reader :num1 :num2 :answer
+  def initialize
+    @num1 = rand(1...20)
+    @num2 = rand(1...20)
+    @answer = @num1 + num2
+  end
+end
