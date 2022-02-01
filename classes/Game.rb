02ea32ee552334a40e565ec5fdc6bd1b287392a0
Michael Dever <mdever@live.ca>
2022-01-31 22:17:28 -0500

    fixed bug where game ended on the turn after someone lost, refactored some code, removed debugging code

--- classes/Game.rb
+++ classes/Game.rb
@@ -11,44 +11,41 @@ class Game
     players << p1
     p2 = Player.new("Player 2")
     players << p2
-    @current_player = p1
-    puts "CURRENT PLAYER #{@current_player}"
-    puts "PLAYER 1 #{p1}"
-    puts "PLAYER 2 #{p2}"
+    @current_player = p2
     while current_player.lives > 0
       round
     end
     game_over
   end
 
-  def round
-    question = Question.new
-    puts "#{current_player.name}: What is #{question.num1} + #{question.num2}"
-    answer = gets.chomp.to_i
-
-    if answer == question.answer
-      puts "#{current_player}: Correct"
-    else
-      puts "#{current_player}: Incorrect"
-      @current_player.lose_life
-    end
-    puts "P1: #{players[0].lives} - P2: #{players[1].lives}"
+  def switch_players
     if @current_player == players[0]
       @current_player = players[1]
     else
       @current_player = players[0]
       
     end
+  end
+  
+  def round
+    switch_players
+    question = Question.new
+    puts "#{current_player.name}: What is #{question.num1} + #{question.num2}?"
+    answer = gets.chomp.to_i
+
+    if answer == question.answer
+      puts "#{@current_player.name}: Correct"
+    else
+      puts "#{@current_player.name}: Incorrect"
+      @current_player.lose_life
+    end
+    puts "P1: #{players[0].lives} - P2: #{players[1].lives}"
+    puts "----- NEW TURN -----"
     @current_round += 1
   end
   
   def game_over
-    if current_player == players[0]
-      current_player = players[1]
-    else
-      current_player = players[0]
-      
-    end
+    switch_players
     puts "#{current_player.name} wins with a score of #{current_player.lives}/3"
     puts "----- GAME OVER -----"
     puts "Good bye!"
